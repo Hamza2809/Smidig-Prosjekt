de71094f1ac3853a563b5bdb8626387f1707e154
Sebbern96 <herrbismo@gmail.com>
2022-05-13 11:19:55 +0200

    lagd skjelett til studyBuddy siden

--- pages/fonts.js
+++ pages/fonts.js
@@ -4,25 +4,30 @@ import TabBar from '../components/TabBar/TabBar';
 export default function FontsPage() {
     return (
         <AppLayout title="atcampus - Fonts">
-            <main className="m-4">
+            <header className="m-4">
                 <h1 className="text-xl lg:text-2xl">StudyBuddy side</h1>
                 <div className="mt-4"></div>
-            </main>
-            <div className="mt-8">
-                <h2 className="text-lg lg:text-xl mb-4">Tab Bar</h2>
+            </header>
 
-                <TabBar
-                    tabs={[
-                        { name: 'Tab 1', selected: true, href: '#1' },
-                        {
-                            name: 'Lorem Ipsum 2',
-                            selected: false,
-                            href: '#2'
-                        },
-                        { name: 'Tab 3', selected: false, href: '#3' }
-                    ]}
-                />
-            </div>
+            <main>
+                <div className="mt-8">
+                    <h2 className="text-lg lg:text-xl mb-4">
+                        her skal info om folk inn
+                    </h2>
+
+                    <TabBar
+                        tabs={[
+                            { name: 'Tab 1', selected: true, href: '#1' },
+                            {
+                                name: 'Lorem Ipsum 2',
+                                selected: false,
+                                href: '#2'
+                            },
+                            { name: 'Tab 3', selected: false, href: '#3' }
+                        ]}
+                    />
+                </div>
+            </main>
         </AppLayout>
     );
 }
