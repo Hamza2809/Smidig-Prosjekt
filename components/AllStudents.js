d64f944dea7ad322fa95435e3748bbc6fddb165d
Sebbern96 <herrbismo@gmail.com>
2022-05-19 12:33:56 +0200

    Begynt på AllStudents.js

--- components/AllStudents.js
+++ components/AllStudents.js
@@ -1,12 +1,49 @@
-import AppLayout from 'components/AppLayout';
+import { useState } from 'react';
 
 export default function AllStudents() {
-  return (
-    <AppLayout title={'atcampus - Oversikt'}>
-      <main className="m-4">
-        <h1 className="text-xl lg:text-2xl"> Oversikt</h1>
-      </main>
-    </AppLayout>
+    const users = useState([
+        {
+            id: 1,
+            firstName: 'Ola',
+            lastName: 'Nordmann',
+            email: 'ola_nordmann@fake.no',
+            emne: 'emne 1',
+            ambisjoner: 'A',
+            arbeidsSted: 'hybrid',
+            arbeidsMengde: '20-40 timer'
+        }
+    ]);
 
-  );
+    return (
+        <div>
+            <h3>React - Display a list of items</h3>
+            <table className="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Emne</th>
+                        <th>Ambisjoner</th>
+                        <th>Arbeids sted</th>
+                        <th>Arbeids mengde</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users &&
+                        users.map((user) => (
+                            <tr key={user.id}>
+                                <td>
+                                    {user.firstName} {user.lastName}
+                                </td>
+                                <td>{user.email}</td>
+                                <td>{user.emne}</td>
+                                <td>{user.ambisjoner}</td>
+                                <td>{user.arbeidsSted}</td>
+                                <td>{user.arbeidsMengde}</td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
