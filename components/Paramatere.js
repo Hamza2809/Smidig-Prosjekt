94a67d56727c37ab6dcd5480b5d59a27877c7d80
Henrikgg <henrik_gg94@hotmail.com>
2022-05-23 10:53:44 +0200

    Update Paramatere.js

--- components/Paramatere.js
+++ components/Paramatere.js
@@ -1,11 +1,65 @@
-import AppLayout from 'components/AppLayout';
+import { useState} from "react";
 
-export default function KollokvieGroup() {
-  return (
-    <AppLayout title={'atcampus - Oversikt'}>
-      <main className="m-4">
-        <h1 className="text-xl lg:text-2xl"> Oversikt</h1>
-      </main>
-    </AppLayout>
-  );
-}
+export default function Paramatere() {
+    const [groups, setGroups] = useState([
+        {
+            id: "1",
+            title: "Kollokvie 1",
+            subtitle: "Frontend og mobilutvikling",
+            description: "Vi er en gruppe..."
+        },
+        {
+            id: "2",
+            title: "Kollokvie 2",
+            subtitle: "Frontend og mobilutvikling",
+            description: "Vi er en gruppe..."
+        },
+        {
+            id: "3",
+            title: "Kollokvie 3",
+            subtitle: "Frontend og mobilutvikling",
+            description: "Vi er en gruppe..."
+        },
+        {
+            id: "4",
+            title: "Kollokvie 43",
+            subtitle: "Frontend og mobilutvikling",
+            description: "Vi er en gruppe..."
+        },
+        {
+            id: "5",
+            title: "Kollokvie 5",
+            subtitle: "Frontend og mobilutvikling",
+            description: "Vi er en gruppe..."
+        }
+    ]);
+
+    const [groupFilter, setGroupFilter] = useState("");
+
+    function updateGroupFilter(event) {
+        setGroupFilter(event.target.value);
+    }
+
+    return (
+        <>
+            <input value={groupFilter} onChange={updateGroupFilter} />
+
+            <div className="flex flex-row flex-wrap">
+                {groups &&
+                    groups
+                        .filter(group => group.title.includes(groupFilter))
+                        .map((group) => (
+                            <div className="card bg-white flex flex-col items-center justify-center p-10 shadow-2xl rounded-2xl w-64 m-4" key={group.id}>
+                                <div className="name text-gray-800 text-xl font-medium mt-4" >
+                                    {group.title}
+                                </div>
+                                <div className="name text-gray-800 text-xl font-medium" >
+                                    {group.subtitle}
+                                </div>
+                            </div>
+                        ))}
+            </div>
+        </>
+    );
+};
+
